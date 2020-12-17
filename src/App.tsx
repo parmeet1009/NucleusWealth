1c30c03d0e5018cb3cd684bbb42e1e6f53a98559
parmeet1009 <36438463+parmeet1009@users.noreply.github.com>
2020-12-17 13:12:28 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see how awesome is PARMEET!</h2>
+    </div>
+  );
+}
